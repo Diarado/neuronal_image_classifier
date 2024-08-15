a68ba7e01c3f4c621304bf31922fd767599e7876
User <User@PATHPS1540801.pathology.columbia.edu>
2024-08-15 14:45:05 -0400

    working version, still needs tuning

---
+++ scripts/test_model.py
@@ -0,0 +1,39 @@
+import os
+from joblib import load
+from scripts.preprocess import preprocess_image
+import pandas as pd
+
+def preprocess_and_flatten(image_path):
+    binary_image = preprocess_image(image_path)
+    return binary_image.flatten()
+
+def test_model(image_dir, model_path='models/classifier_model.pkl', output_csv='predictions.csv'):
+    print('enter')
+    model = load(model_path)
+    image_dir = os.path.join(image_dir, 'images')
+    # List all files in the images directory for debugging
+    all_files = os.listdir(image_dir) 
+    # Gather all .tif files
+    image_paths = [os.path.join(image_dir, file) for file in all_files if file.endswith('.tif')]
+    results = []
+    
+    for image_path in image_paths:
+        print(f"Found image: {image_path}")  # Add this
+
+        processed_image = preprocess_and_flatten(image_path)
+        prediction = model.predict([processed_image])
+        print(f"Processing {image_path}: Prediction = {prediction}")  # Debug statement
+        results.append((os.path.basename(image_path), prediction[0]))
+    
+    if results:
+        df = pd.DataFrame(results, columns=['Image', 'Prediction'])
+        df.to_csv(output_csv, index=False)
+        print(f"Predictions saved to {output_csv}")
+    else:
+        print("No results to save.")
+
+
+if __name__ == "__main__":
+    test_model('data')
+
+
--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,12 +1,14 @@
+import os
+from joblib import dump
 from scripts.parse_csv_to_dict import parse_csv_to_dict
 from scripts.link_images_to_scores import link_images_to_scores
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from joblib import dump
 import numpy as np
 
 def train_in_batches(X, y, batch_size=100):
-    X = np.array(X, dtype=np.float16)  # Convert X to float16 to reduce memory usage
+    X = np.array([img.flatten() for img in X], dtype=np.float16)  # Flatten each image to 1D
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     
     for i in range(0, len(X), batch_size):
@@ -19,6 +21,10 @@ def train_in_batches(X, y, batch_size=100):
 if __name__ == "__main__":
     csv_dict = parse_csv_to_dict('train/scoring_round06.csv')
     X, y = link_images_to_scores('train/round06_images', csv_dict)
-    
     model = train_in_batches(X, y)  # Use batch training
-    dump(model, 'models/classifier_model.pkl')  # Save the trained model
+    
+    # Ensure the models directory exists
+    os.makedirs('models', exist_ok=True)
+    
+    dump(model, 'models/classifier_model.pkl')
+
